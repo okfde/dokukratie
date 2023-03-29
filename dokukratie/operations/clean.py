3a068fc1b72c27e39f0ef88bf916386a70bff992
Simon Wörpel <simon.woerpel@pm.me>
2023-03-29 14:50:28 +0200

    Handle missing reference key

--- dokukratie/operations/clean.py
+++ dokukratie/operations/clean.py
@@ -2,6 +2,10 @@ from memorious_extended.operations import clean as _clean
 
 
 def clean(context, data, emit=True):
+    if "reference" not in data:
+        context.log.warn("No reference for `%s`" % data["url"])
+        return
+
     # rewrite document types
     document_types = {v: k for k, v in context.crawler.config["document_types"].items()}
     data["document_type"] = document_types[data["document_type"]]
